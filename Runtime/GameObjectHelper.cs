a730ec1c8339a632babde6c4a4e49d1f82b1fec7
zcp <1097595778@qq.com>
2024-07-31 13:14:49 +0800

    update

--- Runtime/GameObjectHelper.cs
+++ Runtime/GameObjectHelper.cs
@@ -2,16 +2,16 @@ using System;
 using UnityEngine;
 
 public static class GameObjectHelper
+{
+    public static T Get<T>(this GameObject gameObject, string key) where T : class
     {
-        public static T Get<T>(this GameObject gameObject, string key) where T : class
+        try
         {
-            try
-            {
-                return gameObject.GetComponent<ReferenceCollector>().Get<T>(key);
-            }
-            catch (Exception e)
-            {
-                throw new Exception($"获取{gameObject.name}的ReferenceCollector key失败, key: {key}", e);
-            }
+            return gameObject.GetComponent<ReferenceCollector>().Get<T>(key);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"获取{gameObject.name}的ReferenceCollector key失败, key: {key}", e);
         }
     }
+}
